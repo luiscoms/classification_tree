8c7170ab41336f71c9aabc1fa64bf04e4b9211fb
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-11-29 18:24:13 -0200

    Update license

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 __version__ = '0.0.0'
 __repo__ = 'http://gitlab.rbs.com.br/rbsdev/classification_tree'
 
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+
 with open('README.md') as f:
     readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
 
 setup(
     name='classification_tree',
@@ -20,7 +22,8 @@ setup(
     author_email='luiscoms@ateliedocodigo.com.br',
     url=__repo__,
     download_url='{}/tarball/{}'.format(__repo__, __version__),
-    locence=license,
+    install_requires=reqs,
+    licence='MIT',
     keywords=[],
     classifiers=[],
     packages=find_packages(exclude=('tests', 'docs'))
